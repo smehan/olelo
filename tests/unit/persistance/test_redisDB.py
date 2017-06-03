8e4abd0cdf4fc8cca29bfe85b1bc7d122b3f0032
Shawn Mehan <shawn.mehan@ieee.org>
2017-06-02 17:26:07 -0700

    More tests. Would like to have a teardown for cleaning up test keys.

--- tests/unit/persistance/test_redisDB.py
+++ tests/unit/persistance/test_redisDB.py
@@ -15,9 +15,24 @@ from persistance.redis_db import RedisDB
 
 @pytest.fixture(scope='class')
 def red():
-    print("Making a RedisDB instance")
+    """
+    create a redis connection object
+    :return: 
+    """
+    print("\nMaking a RedisDB instance")
     r = RedisDB()
     return r
+
+@pytest.fixture(scope='function')
+def make_test_hash(rdb):
+    """
+    
+    :param rdb: RedisDB connection object
+    :return: 
+    """
+    h = rdb._add_key_to_hash('test_hash', 'test_key')
+    return h
+
 
 
 @pytest.mark.usefixtures('red')
@@ -32,19 +47,35 @@ class TestRedisDB(object):
         assert r.decode_s(b'abc') == 'abc'
 
     def test__add_key_to_hash(self):
-        self.fail()
+        r = red()
+        with pytest.raises(ValueError):
+            r._add_key_to_hash(None, None)
+        assert r._add_key_to_hash('test_hash', None) == None
+        assert r._add_key_to_hash('test_hash', 'test_key') == 'test_hash:id'
 
     def test__all_keys_from_hash(self):
-        self.fail()
+        r = red()
+        h = make_test_hash(r)
+        assert r._all_keys_from_hash(h) == ['test_key']
 
     def test__all_values_from_hash(self):
-        self.fail()
+        r = red()
+        h = make_test_hash(r)
+        assert r._all_values_from_hash(h) == ['00942f4668670f34c5943cf52c7ef3139fe2b8d6']
 
     def test__all_hash(self):
-        self.fail()
+        r = red()
+        h = make_test_hash(r)
+        assert r._all_hash(h) == {'test_key': '00942f4668670f34c5943cf52c7ef3139fe2b8d6'}
 
     def test__v_from_hash(self):
-        self.fail()
+        r = red()
+        h = make_test_hash(r)
+        assert r._v_from_hash(h, 'test_key') == '00942f4668670f34c5943cf52c7ef3139fe2b8d6'
 
     def test__add_to_set(self):
-        self.fail()
+        r = red()
+        assert r._add_to_set('test_set',
+                             '00942f4668670f34c5943cf52c7ef3139fe2b8d6',
+                             ['a', 'b', 'c']) == b'test_set:00942f4668670f34c5943cf52c7ef3139fe2b8d6'
+
