a583afe45bd5b1168552290e93d68238af0e7fac
Shawn Mehan <shawn.mehan@ieee.org>
2017-07-17 10:50:34 -0700

    modern mamaka sources.

--- twitter/tweeter.py
+++ twitter/tweeter.py
@@ -42,7 +42,7 @@ class Tweeter(RedisDB):
 
     def push_tweet(self, hw, defs):
         api = self.API
-        tweet = f"#Hawaiian Word Of The Day: {hw} - {defs}"
+        tweet = f"#Hawaiian WOTD: {hw} - {defs}"
         if self.DEBUG:
             print(f'About to tweet: {tweet}')
         else:
