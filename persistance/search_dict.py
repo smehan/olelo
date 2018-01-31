ef81f2114fb50c62ed8e70f791dffe8b296de29c
Shawn Mehan <shawn.mehan@ieee.org>
2018-01-31 13:57:45 -0800

    Add hw to search method

--- persistance/search_dict.py
+++ persistance/search_dict.py
@@ -27,19 +27,21 @@ class RedisSearch(RedisDB):
     def __repr__(self):
         return f'<RedisSearch()'
 
-    def find_hw(self, huid):
+    def find_hw(self, huid=None, hw=None):
         """
-        Given a valid HUID, will return the hawaiian hw
+        Given a valid HUID or Hawaiian headword,
+        will return the hawaiian hw
         that is paired with that HUID as well as
         definitions and pos.
-        :param huid:
+        :param huid: HUID token
+        :param hw: HAW headword string
         :return:
         """
         hw_hash = self._all_hash('haw:id')
         for k, v in hw_hash.items():
-            if v == huid:
-                word_defs = self._all_values_from_hash(':'.join(['defs', huid]))
-                word_pos = self.rdb.smembers(':'.join(['pos', huid]))
+            if (huid and v == huid) or (hw and k == hw):
+                word_defs = self._all_values_from_hash(':'.join(['defs', v]))
+                word_pos = self.rdb.smembers(':'.join(['pos', v]))
                 print(f'HW - {k}, HUID - {v}')
                 print(f'Defs - {word_defs}')
                 print(f'POS - {word_pos}')
@@ -47,4 +49,5 @@ class RedisSearch(RedisDB):
 
 if __name__ == '__main__':
     haw_searcher = RedisSearch()
-    haw_searcher.find_hw('5bb6fffeffef6622eafe0e99c410b2fc9bf50cc5')
+    haw_searcher.find_hw(huid='5bb6fffeffef6622eafe0e99c410b2fc9bf50cc5')
+    haw_searcher.find_hw(hw='ana')
