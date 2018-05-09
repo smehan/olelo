2589982b906df457e95986f23b6d47a9b648aaf9
Shawn Mehan <shawn.mehan@ieee.org>
2018-05-09 11:12:22 -0700

    Updates doc

---
+++ oleloweb/oleloweb.py
@@ -0,0 +1,15 @@
+###########################################################
+# Copyright (C) 2018 Shawn Mehan <shawn dot mehan at shawnmehan dot com>
+# Main module for olelo flask web app
+###########################################################
+#
+#  -*- coding: utf-8 -*-
+
+# standard libs
+
+# 3rd-party libs
+from flask import Flask
+
+# application libs
+
+
